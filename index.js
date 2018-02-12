80e84b0bbde3d97a448a89eb45431392426e26f9
kvnamipara <kvnamipara@gmail.com>
2018-02-13 02:15:35 +0530

    form completion

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const bodyparser = require('body-parser');
 
 app.set('view engine', 'hbs');
+app.use(bodyparser.json());
 app.use(express.static(__dirname + '/public'));
 // app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
@@ -13,8 +15,9 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/', (req, res) => {
+app.post('/submit', (req, res) => {
   console.log(req.body);
+  alert("ok");
 });
 
 server.listen(port, () => {
