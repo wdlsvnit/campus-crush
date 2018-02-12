2c026d86a7bb1698811d9ad7421969a00c92b952
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-02-13 03:38:10 +0530

    Save users to database

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 const server = require('http').Server(app);
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const user = require('./models/user.js');
+const user = require('./models/user');
+const bodyParser = require('body-parser');
 
 // console.log(user);
 
+// mongoose.connect('mongodb://kevin:kevin@ds129706.mlab.com:29706/campus-crush');
 mongoose.connect('mongodb://sumedh:campus-crush@ds129706.mlab.com:29706/campus-crush');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded());
 // app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', (req, res) => {
@@ -20,10 +23,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  console.log('post received');
   let newMajnu = new user();
-  newMajnu.firstname = 'sumedh';
-  console.log(newMajnu);
+  newMajnu.name = req.body.name;
+  newMajnu.your_fb = req.body.ownfblink;
+  newMajnu.crush_fb = req.body.crushfblink;
+  newMajnu.mobile_no = req.body.ownconumber;
+  newMajnu.created_at = new Date();
+  newMajnu.save((err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data);
+  });
   res.render('index');
 });
 
--- models/user.js
+++ models/user.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userschema = new Schema({
+	// firstname : { type : String, required : true },
 	name : { type : String, required : true },
-	lastname : { type : String, required : true },
   your_fb : { type : String, required : true },
   crush_fb : { type : String, required : true },
+	mobile_no: { type: Number, required: true },
 	created_at : Date
 });
 
-const User = mongoose.model('User', userschema);
+const User = mongoose.model('user', userschema);
 module.exports = User;
