2e5b238b66501b3188172cf86fcb2a94d2f420e8
Sumedh Nimkarde <blesssumedh@gmail.com>
2018-02-13 01:40:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+// app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
